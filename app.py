ce0739600200d751534a6137051dd08f59189287
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2022-12-21 11:53:38 +0000

    push modal update

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask,redirect,url_for,render_template,request, send_from_directory, current_app
+import os
 
 app=Flask(__name__)
 @app.route('/home',methods=['GET','POST'])
 def home():
     return render_template('index.html')
 
+app.config['UPLOAD_FOLDER']='Documents'
+
+
 @app.route('/')
 def online():
     return render_template('online.html', title="Online Application Form.")
+
+@app.route('/downloadOnlineManual', methods=['GET', 'POST'])
+def downloadOnlineManual():
+    path = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    print(path)
+    filename = "online_forms_manual.pdf"
+    return send_from_directory(path, filename, as_attachment=True)
 
 @app.route('/post')
 def post():
