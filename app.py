8a41fa616ead2f098fffef6835fc6a0a9cdf176d
Kweku7k <mr.adumatta@gmail.com>
2024-05-02 23:48:35 +0000

    ii

--- app.py
+++ app.py
@@ -440,7 +440,7 @@ def home():
     print(category_form_url)
 
     try:
-        category = requests.get(category_form_url).json()
+        category = requests.get(category_form_url, verify=False).json()
         print("category")
         print(category)
         form.category.choices = category
