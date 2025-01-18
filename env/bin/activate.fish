f7a23049c261ad202153f2000cd26e9d726a1396
SoftLife0 <onikosiadewale18@gmail.com>
2025-01-18 11:55:58 +0000

    update virtual environment paths and replace outdated links in templates

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nanakweku/Documents/Projects/CentralUniversity/cuweb/env"
+set -gx VIRTUAL_ENV "/Users/mrsoftlife/Documents/Projects/cuweb/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
